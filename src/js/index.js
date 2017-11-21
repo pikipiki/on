731d8340172b658e976232746f15b2879d924122
pikipiki <franky.kp@gmail.com>
2017-11-21 17:36:27 +0100

    feat: can hover dropdown menus

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,3 @@
 $(document).ready(() => {
 
-  $('.link-dropdown').on('mouseover',function() {
-    $(this).siblings().addClass('active')  
-  })
-
-  $('.link-dropdown').on('mouseleave',function() {
-    $(this).siblings().removeClass('active')  
-  })
-
 })
